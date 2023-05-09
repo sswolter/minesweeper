a38dba3866c537dba473ff3398c0a72a47c68915
sswolter <121854262+sswolter@users.noreply.github.com>
2023-05-09 10:03:41 +1000

    Add files via upload

--- minesweeper/src/minesweeper/Game.java
+++ minesweeper/src/minesweeper/Game.java
@@ -4,30 +4,33 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Game {
+    static Board board = new Board();
+    static Scanner scanner = new Scanner(System.in);
+
+
+    static boolean gameOver = false;
+
     public static void main(String[] args) {
 
-        Board board = new Board();
-        Scanner scanner = new Scanner(System.in);
-
-
-        boolean gameOver = false;
         
         board.printGrid();
         
         while (!gameOver) {
 
-        	System.out.println("Enter the column and row sperated by a space: ");
+        	System.out.println();
         	int col; 
         	int row;
 
         	try { 
+            	System.out.print("Row: ");
+            	row = scanner.nextInt();
+            	System.out.print("Column: ");
         		col = scanner.nextInt();
-        		row = scanner.nextInt();
 
         		if (col < 0 || col > board.numberOfColumns - 1 || row < 0 || row > board.numberOfRows - 1) {
         			System.out.println("Please enter a number between 0 and " + (board.numberOfColumns - 1) + ". \n");
         			continue;
-        		}
+        			}
         		} catch (InputMismatchException e) {
         			System.out.println("Invalid input. Please enter a number. \n");
         			scanner.next();
